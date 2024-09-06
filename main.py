964118622c7804c7cf2a16c55b97c402da976b5d
kondo takeshi <chaspy@users.noreply.github.com>
2024-09-06 15:48:29 +0900

    Run with Datadog

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langfuse.callback import CallbackHandler
+from ddtrace.llmobs import LLMObs
 
 # langfuse initial setting
 langfuse_handler = CallbackHandler(
     secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
     public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
     host=os.getenv("LANGFUSE_HOST"),
+)
+
+# Datadog LLM Observability
+LLMObs.enable(
+    ml_app=os.getenv("DD_LLMOBS_ML_APP"),
+    api_key=os.getenv("DD_API_KEY"),
+    site=os.getenv("DD_SITE"),
+    agentless_enabled=True,
 )
 
 
